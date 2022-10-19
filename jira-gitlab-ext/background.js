53574d0ae55ae68638e29c4e7c3cf54f3cf7b05e
Esther <66259954+theansweris101010@users.noreply.github.com>
2022-10-19 16:40:54 +0300

    Remove debugger and unused console log

--- jira-gitlab-ext/background.js
+++ jira-gitlab-ext/background.js
@@ -18,10 +18,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-      debugger
       if (tab.url != "" && tab.url.includes('.atlassian.net/browse/')){
         chrome.tabs.executeScript(tab.id, {file: 'content-script.js'}, ([results]) => {
-          console.log(results);
         });
       }
   });
